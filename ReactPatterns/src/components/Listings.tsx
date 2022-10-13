36fccbaa23c7d42e33258cbdce643e0825a6dce7
Daria Platonova <plat-dasha@mail.ru>
2022-10-13 17:21:24 +0300

    added hoc pattern

---
+++ ReactPatterns/src/components/Listings.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import withLoader from '../hoc/withLoader';
+
+import { Listing } from './List';
+import { ListingsGrid } from './ListingsGrid';
+
+export function ListingsLayout({ data }) {
+  return (
+    <ListingsGrid>
+      {data.map((listing, i) => (
+        <Listing listing={listing} key={i}/>
+      ))}
+    </ListingsGrid>
+  );
+}
+
+const Listings = withLoader(ListingsLayout, 'https://house-lydiahallie.vercel.app/api/listings');
+export default Listings;
---
+++ ReactPatterns/src/hoc/withLoader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import useSWR from "swr";
+
+import { LoadingSpinner } from "../components/LoadingSpinner";
+
+export default function withLoader(Element, url) {
+  return (props) => {
+    const { data } = useSWR(url, (link) => fetch(link).then((res) => res.json()));
+    return data ? <Element {...props} data={data.listings} /> : <LoadingSpinner />;
+  };
+}
